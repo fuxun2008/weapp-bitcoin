dda1478c4f616666ad6e287e7413949c0e0204d9
Xun.Fu <fuxun@xiaoyouzi.com>
2017-06-10 22:25:34 +0800

    完成资讯首页UI。

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,26 +1,260 @@
 //index.js
 //获取应用实例
-var app = getApp()
+import _ from '../../utils/util';
+var app = getApp();
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {}
+    currentTab: 1,
+    tabs: [
+      {
+        id: 1,
+        name: '科普入门'
+      },
+      {
+        id: 2,
+        name: '最新新闻'
+      },
+      {
+        id: 3,
+        name: '进阶'
+      },
+      {
+        id: 4,
+        name: '操作比特'
+      }
+    ],
+    swiper: {
+      imgUrls: [{
+          id: 1,
+          url: 'http://m.anhuinews.com/upload/2017/06/09/20176912333117.jpg',
+          desc: '合肥不夜书店的24小时：书里的诗意和远方'
+        }, {
+          id: 2,
+          url: 'http://m.anhuinews.com/upload/2017/06/10/20176106404326.jpg',
+          desc: '塌陷区建成漂浮电站'
+        }, {
+          id: 3,
+          url: 'http://m.anhuinews.com/upload/2017/06/08/2017681213020.jpg',
+          desc: '中安在线池州频道开通上线(组图)'
+        }, {
+          id: 4,
+          url: 'http://m.anhuinews.com/upload/2017/06/09/2017695504933.png',
+          desc: '合安高铁开始架梁 建成后合肥到安庆40分钟'
+        }
+      ],
+      indicatorDots: false,
+      autoplay: true,
+      circular: true,
+      interval: 3000,
+      duration: 500
+    },
+    articles: [
+      {
+        id: 10,
+        title: '比特币触底反弹，澳交所接受使用比特币支付收购款',
+        img: 'http://m.anhuinews.com/upload/2017/06/06/201766631967.jpg',
+        resource: 'GoogleNews',
+        timestamp: _.dateFromNow(1497082010307),
+        count: 1234
+      }, {
+        id: 11,
+        title: '十分钟看懂比特币的过去、现在和未来',
+        img: 'http://m.anhuinews.com/upload/2017/06/10/2017610673781.jpg',
+        resource: '搜狐新闻',
+        timestamp: _.dateFromNow(1497082020307),
+        count: 1211
+      }, {
+        id: 12,
+        title: '比特币场内平台升级反洗钱系统场外监管政策空白有待填补',
+        img: 'http://m.anhuinews.com/upload/2017/06/06/201766631967.jpg',
+        resource: '外汇宝',
+        timestamp: _.dateFromNow(1497073010801),
+        count: 111
+      }, {
+        id: 13,
+        title: '比特币场内平台升级反洗钱系统场外监管政策空白有待填补',
+        img: 'http://m.anhuinews.com/upload/2017/06/06/201766631967.jpg',
+        resource: '外汇宝',
+        timestamp: _.dateFromNow(1497073010801),
+        count: 111
+      }, {
+        id: 14,
+        title: '比特币场内平台升级反洗钱系统场外监管政策空白有待填补',
+        img: 'http://m.anhuinews.com/upload/2017/06/06/201766631967.jpg',
+        resource: '外汇宝',
+        timestamp: _.dateFromNow(1497073010801),
+        count: 111
+      }, {
+        id: 15,
+        title: '比特币场内平台升级反洗钱系统场外监管政策空白有待填补',
+        img: 'http://m.anhuinews.com/upload/2017/06/06/201766631967.jpg',
+        resource: '外汇宝',
+        timestamp: _.dateFromNow(1497073010801),
+        count: 111
+      }, {
+        id: 16,
+        title: '比特币场内平台升级反洗钱系统场外监管政策空白有待填补',
+        img: 'http://m.anhuinews.com/upload/2017/06/06/201766631967.jpg',
+        resource: '外汇宝',
+        timestamp: _.dateFromNow(1497073010801),
+        count: 111
+      }, {
+        id: 17,
+        title: '比特币场内平台升级反洗钱系统场外监管政策空白有待填补',
+        img: 'http://m.anhuinews.com/upload/2017/06/06/201766631967.jpg',
+        resource: '外汇宝',
+        timestamp: _.dateFromNow(1497073010801),
+        count: 111
+      }, {
+        id: 18,
+        title: '比特币场内平台升级反洗钱系统场外监管政策空白有待填补',
+        img: 'http://m.anhuinews.com/upload/2017/06/06/201766631967.jpg',
+        resource: '外汇宝',
+        timestamp: _.dateFromNow(1497073010801),
+        count: 111
+      }, {
+        id: 19,
+        title: '比特币场内平台升级反洗钱系统场外监管政策空白有待填补',
+        img: 'http://m.anhuinews.com/upload/2017/06/06/201766631967.jpg',
+        resource: '外汇宝',
+        timestamp: _.dateFromNow(1497073010801),
+        count: 111
+      }, {
+        id: 20,
+        title: '比特币场内平台升级反洗钱系统场外监管政策空白有待填补',
+        img: 'http://m.anhuinews.com/upload/2017/06/06/201766631967.jpg',
+        resource: '外汇宝',
+        timestamp: _.dateFromNow(1497073010801),
+        count: 111
+      }, {
+        id: 21,
+        title: '比特币场内平台升级反洗钱系统场外监管政策空白有待填补',
+        img: 'http://m.anhuinews.com/upload/2017/06/06/201766631967.jpg',
+        resource: '外汇宝',
+        timestamp: _.dateFromNow(1497073010801),
+        count: 111
+      }, {
+        id: 22,
+        title: '比特币场内平台升级反洗钱系统场外监管政策空白有待填补',
+        img: 'http://m.anhuinews.com/upload/2017/06/06/201766631967.jpg',
+        resource: '外汇宝',
+        timestamp: _.dateFromNow(1497073010801),
+        count: 111
+      }, {
+        id: 23,
+        title: '比特币场内平台升级反洗钱系统场外监管政策空白有待填补',
+        img: 'http://m.anhuinews.com/upload/2017/06/06/201766631967.jpg',
+        resource: '外汇宝',
+        timestamp: _.dateFromNow(1497073010801),
+        count: 111
+      }, {
+        id: 24,
+        title: '比特币场内平台升级反洗钱系统场外监管政策空白有待填补',
+        img: 'http://m.anhuinews.com/upload/2017/06/06/201766631967.jpg',
+        resource: '外汇宝',
+        timestamp: _.dateFromNow(1497073010801),
+        count: 111
+      }, {
+        id: 25,
+        title: '比特币场内平台升级反洗钱系统场外监管政策空白有待填补',
+        img: 'http://m.anhuinews.com/upload/2017/06/06/201766631967.jpg',
+        resource: '外汇宝',
+        timestamp: _.dateFromNow(1497073010801),
+        count: 111
+      }, {
+        id: 26,
+        title: '比特币场内平台升级反洗钱系统场外监管政策空白有待填补',
+        img: 'http://m.anhuinews.com/upload/2017/06/06/201766631967.jpg',
+        resource: '外汇宝',
+        timestamp: _.dateFromNow(1497073010801),
+        count: 111
+      }, {
+        id: 27,
+        title: '比特币场内平台升级反洗钱系统场外监管政策空白有待填补',
+        img: 'http://m.anhuinews.com/upload/2017/06/06/201766631967.jpg',
+        resource: '外汇宝',
+        timestamp: _.dateFromNow(1497073010801),
+        count: 111
+      }, {
+        id: 28,
+        title: '比特币场内平台升级反洗钱系统场外监管政策空白有待填补',
+        img: 'http://m.anhuinews.com/upload/2017/06/06/201766631967.jpg',
+        resource: '外汇宝',
+        timestamp: _.dateFromNow(1497073010801),
+        count: 111
+      }, {
+        id: 29,
+        title: '比特币场内平台升级反洗钱系统场外监管政策空白有待填补',
+        img: 'http://m.anhuinews.com/upload/2017/06/06/201766631967.jpg',
+        resource: '外汇宝',
+        timestamp: _.dateFromNow(1497073010801),
+        count: 111
+      }, {
+        id: 30,
+        title: '比特币场内平台升级反洗钱系统场外监管政策空白有待填补',
+        img: 'http://m.anhuinews.com/upload/2017/06/06/201766631967.jpg',
+        resource: '外汇宝',
+        timestamp: _.dateFromNow(1497073010801),
+        count: 111
+      }, {
+        id: 31,
+        title: '比特币场内平台升级反洗钱系统场外监管政策空白有待填补',
+        img: 'http://m.anhuinews.com/upload/2017/06/06/201766631967.jpg',
+        resource: '外汇宝',
+        timestamp: _.dateFromNow(1497073010801),
+        count: 111
+      }, {
+        id: 32,
+        title: '比特币场内平台升级反洗钱系统场外监管政策空白有待填补',
+        img: 'http://m.anhuinews.com/upload/2017/06/06/201766631967.jpg',
+        resource: '外汇宝',
+        timestamp: _.dateFromNow(1497073010801),
+        count: 111
+      }, {
+        id: 33,
+        title: '比特币场内平台升级反洗钱系统场外监管政策空白有待填补',
+        img: 'http://m.anhuinews.com/upload/2017/06/06/201766631967.jpg',
+        resource: '外汇宝',
+        timestamp: _.dateFromNow(1497073010801),
+        count: 111
+      }
+    ],
+    errorMsg: '暂时没有数据哦~',
+    hasData: true,
+    hasMore: false,
+    showLoading: false
+  },
+  onShareAppMessage: function() {
+    return {
+      title: '自定义转发标题',
+      path: '/page/index/index',
+      success: function(res) {
+        // 转发成功
+        console.log(res);
+      },
+      fail: function(res) {
+        // 转发失败
+        console.log(res.errMsg);
+      }
+    };
   },
   //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
+  bindViewTap: function(e) {
+    const that = this;
+    const id = parseInt(e.currentTarget.dataset.id, 10);
+    that.setData({
+      currentTab: id
+    });
   },
   onLoad: function () {
-    console.log('onLoad')
-    var that = this
+    console.log('onLoad');
+    var that = this;
     //调用应用实例的方法获取全局数据
     app.getUserInfo(function(userInfo){
       //更新数据
       that.setData({
         userInfo:userInfo
-      })
-    })
+      });
+    });
   }
-})
+});
--- utils/util.js
+++ utils/util.js
@@ -1,21 +1,85 @@
-function formatTime(date) {
-  var year = date.getFullYear()
-  var month = date.getMonth() + 1
-  var day = date.getDate()
+/**
+ * 时间格式转换 ms -> date
+ * @return {string} yyyy-MM-dd hh:mm:ss格式时间
+ */
+const msToDate = (_ms, _format) => {
+  let ms = _ms;
+  let format = _format;
+  if (ms.toString().length === 10) {
+    ms *= 1000;
+  }
+  format = format || 'yyyy-MM-dd hh:mm:ss';
+  const d = new Date(ms);
+  const year = d.getFullYear();
+  const month = d.getMonth() + 1;
+  const day = d.getDate();
+  const hour = d.getHours();
+  const minute = d.getMinutes();
+  const seconds = d.getSeconds();
 
-  var hour = date.getHours()
-  var minute = date.getMinutes()
-  var second = date.getSeconds()
+  const addPrefix = source => (source < 10 ? `0${source}` : source);
 
+  format = format.replace('yyyy', year)
+    .replace('MM', addPrefix(month))
+    .replace('dd', addPrefix(day))
+    .replace('hh', addPrefix(hour))
+    .replace('mm', addPrefix(minute))
+    .replace('ss', addPrefix(seconds));
 
-  return [year, month, day].map(formatNumber).join('/') + ' ' + [hour, minute, second].map(formatNumber).join(':')
-}
+  return format;
+};
 
-function formatNumber(n) {
-  n = n.toString()
-  return n[1] ? n : '0' + n
-}
+/**
+ * 时间格式转换 ms -> date
+ * @return {string} yyyy-MM-dd hh:mm:ss格式时间
+ */
+const nowToDate = _format => {
+  let format = _format;
+  format = format || 'yyyy-MM-dd hh:mm:ss';
+  return msToDate(new Date().getTime(), format);
+};
+
+/**
+ * 过去距离当前时间差
+ * @param  {int} ms 毫秒数
+ * @return {string}    时间差标示
+ */
+const dateFromNow = ms => {
+  const time = parseFloat(ms) / 1000;
+  let result = '';
+  if (time) {
+    if (time > 60 && time < 3600) {
+      result = `${parseInt(time / 60.0, 10)}分钟前`;
+    } else if (time >= 3600 && time < 24 * 3600) {
+      result = `${parseInt(time / 3600.0, 10)}小时前`;
+    } else if (time >= 24 * 3600 && time < 30 * 24 * 3600) {
+      result = `${parseInt(time / 3600.0 / 24.0, 10)}天前`;
+    } else if (time >= 30 * 24 * 3600 && time < 12 * 30 * 24 * 3600) {
+      result = `${parseInt(time / 3600.0 / 24.0 / 30, 10)}月前`;
+    } else if (time >= 12 * 30 * 24 * 3600) {
+      result = `${parseInt(time / 3600.0 / 24.0 / 30.0 / 12.0, 10)}年前`;
+    } else {
+      result = `${parseInt(time / 1.0, 10)}秒前`;
+    }
+  }
+
+  return result;
+};
+
+/**
+ * object转成uri参数字符串
+ * @param  {object} obj object格式数据 { x: xx, y: yy }
+ * @return {string}     uri参数字串 x=xx&y=yy
+ */
+const obj2uri = (obj) => {
+  return Object.keys(obj).map(function (k) {
+    return encodeURIComponent(k) + '=' + encodeURIComponent(obj[k]);
+  }).join('&');
+};
 
 module.exports = {
-  formatTime: formatTime
-}
+  msToDate,
+  nowToDate,
+  dateFromNow,
+  obj2uri
+};
