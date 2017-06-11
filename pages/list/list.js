4fe261e2debd499f2ee3cd4d214da837b87b17f6
Xun.Fu <fuxun@xiaoyouzi.com>
2017-06-11 21:54:12 +0800

    完成列表页UI。

---
+++ pages/list/list.js
@@ -0,0 +1,198 @@
+//list.js
+import _ from '../../utils/util.js';
+
+Page({
+  data: {
+    articles: [
+      {
+        id: 10,
+        title: '比特币触底反弹，澳交所接受使用比特币支付收购款',
+        img: 'http://m.anhuinews.com/upload/2017/06/06/201766631967.jpg',
+        resource: 'GoogleNews',
+        timestamp: _.dateFromNow(1497082010307),
+        count: 1234
+      }, {
+        id: 11,
+        title: '十分钟看懂比特币的过去、现在和未来',
+        img: 'http://m.anhuinews.com/upload/2017/06/10/2017610673781.jpg',
+        resource: '搜狐新闻',
+        timestamp: _.dateFromNow(1497082020307),
+        count: 1211
+      }, {
+        id: 12,
+        title: '比特币场内平台升级反洗钱系统场外监管政策空白有待填补比特币场内平台升级反洗钱系统场外监管政策空白有待填补',
+        img: 'http://m.anhuinews.com/upload/2017/06/06/201766631967.jpg',
+        resource: '外汇宝',
+        timestamp: _.dateFromNow(1497073010801),
+        count: 111
+      }, {
+        id: 13,
+        title: '比特币场内平台升级反洗钱系统场外监管政策空白有待填补',
+        img: 'http://m.anhuinews.com/upload/2017/06/06/201766631967.jpg',
+        resource: '外汇宝',
+        timestamp: _.dateFromNow(1497073010801),
+        count: 111
+      }, {
+        id: 14,
+        title: '比特币场内平台升级反洗钱系统场外监管政策空白有待填补',
+        img: 'http://m.anhuinews.com/upload/2017/06/06/201766631967.jpg',
+        resource: '外汇宝',
+        timestamp: _.dateFromNow(1497073010801),
+        count: 111
+      }, {
+        id: 15,
+        title: '比特币场内平台升级反洗钱系统场外监管政策空白有待填补',
+        img: 'http://m.anhuinews.com/upload/2017/06/06/201766631967.jpg',
+        resource: '外汇宝',
+        timestamp: _.dateFromNow(1497073010801),
+        count: 111
+      }, {
+        id: 16,
+        title: '比特币场内平台升级反洗钱系统场外监管政策空白有待填补',
+        img: 'http://m.anhuinews.com/upload/2017/06/06/201766631967.jpg',
+        resource: '外汇宝',
+        timestamp: _.dateFromNow(1497073010801),
+        count: 111
+      }, {
+        id: 17,
+        title: '比特币场内平台升级反洗钱系统场外监管政策空白有待填补',
+        img: 'http://m.anhuinews.com/upload/2017/06/06/201766631967.jpg',
+        resource: '外汇宝',
+        timestamp: _.dateFromNow(1497073010801),
+        count: 111
+      }, {
+        id: 18,
+        title: '比特币场内平台升级反洗钱系统场外监管政策空白有待填补',
+        img: 'http://m.anhuinews.com/upload/2017/06/06/201766631967.jpg',
+        resource: '外汇宝',
+        timestamp: _.dateFromNow(1497073010801),
+        count: 111
+      }, {
+        id: 19,
+        title: '比特币场内平台升级反洗钱系统场外监管政策空白有待填补',
+        img: 'http://m.anhuinews.com/upload/2017/06/06/201766631967.jpg',
+        resource: '外汇宝',
+        timestamp: _.dateFromNow(1497073010801),
+        count: 111
+      }, {
+        id: 20,
+        title: '比特币场内平台升级反洗钱系统场外监管政策空白有待填补',
+        img: 'http://m.anhuinews.com/upload/2017/06/06/201766631967.jpg',
+        resource: '外汇宝',
+        timestamp: _.dateFromNow(1497073010801),
+        count: 111
+      }, {
+        id: 21,
+        title: '比特币场内平台升级反洗钱系统场外监管政策空白有待填补',
+        img: 'http://m.anhuinews.com/upload/2017/06/06/201766631967.jpg',
+        resource: '外汇宝',
+        timestamp: _.dateFromNow(1497073010801),
+        count: 111
+      }, {
+        id: 22,
+        title: '比特币场内平台升级反洗钱系统场外监管政策空白有待填补',
+        img: 'http://m.anhuinews.com/upload/2017/06/06/201766631967.jpg',
+        resource: '外汇宝',
+        timestamp: _.dateFromNow(1497073010801),
+        count: 111
+      }, {
+        id: 23,
+        title: '比特币场内平台升级反洗钱系统场外监管政策空白有待填补',
+        img: 'http://m.anhuinews.com/upload/2017/06/06/201766631967.jpg',
+        resource: '外汇宝',
+        timestamp: _.dateFromNow(1497073010801),
+        count: 111
+      }, {
+        id: 24,
+        title: '比特币场内平台升级反洗钱系统场外监管政策空白有待填补',
+        img: 'http://m.anhuinews.com/upload/2017/06/06/201766631967.jpg',
+        resource: '外汇宝',
+        timestamp: _.dateFromNow(1497073010801),
+        count: 111
+      }, {
+        id: 25,
+        title: '比特币场内平台升级反洗钱系统场外监管政策空白有待填补',
+        img: 'http://m.anhuinews.com/upload/2017/06/06/201766631967.jpg',
+        resource: '外汇宝',
+        timestamp: _.dateFromNow(1497073010801),
+        count: 111
+      }, {
+        id: 26,
+        title: '比特币场内平台升级反洗钱系统场外监管政策空白有待填补',
+        img: 'http://m.anhuinews.com/upload/2017/06/06/201766631967.jpg',
+        resource: '外汇宝',
+        timestamp: _.dateFromNow(1497073010801),
+        count: 111
+      }, {
+        id: 27,
+        title: '比特币场内平台升级反洗钱系统场外监管政策空白有待填补',
+        img: 'http://m.anhuinews.com/upload/2017/06/06/201766631967.jpg',
+        resource: '外汇宝',
+        timestamp: _.dateFromNow(1497073010801),
+        count: 111
+      }, {
+        id: 28,
+        title: '比特币场内平台升级反洗钱系统场外监管政策空白有待填补',
+        img: 'http://m.anhuinews.com/upload/2017/06/06/201766631967.jpg',
+        resource: '外汇宝',
+        timestamp: _.dateFromNow(1497073010801),
+        count: 111
+      }, {
+        id: 29,
+        title: '比特币场内平台升级反洗钱系统场外监管政策空白有待填补',
+        img: 'http://m.anhuinews.com/upload/2017/06/06/201766631967.jpg',
+        resource: '外汇宝',
+        timestamp: _.dateFromNow(1497073010801),
+        count: 111
+      }, {
+        id: 30,
+        title: '比特币场内平台升级反洗钱系统场外监管政策空白有待填补',
+        img: 'http://m.anhuinews.com/upload/2017/06/06/201766631967.jpg',
+        resource: '外汇宝',
+        timestamp: _.dateFromNow(1497073010801),
+        count: 111
+      }, {
+        id: 31,
+        title: '比特币场内平台升级反洗钱系统场外监管政策空白有待填补',
+        img: 'http://m.anhuinews.com/upload/2017/06/06/201766631967.jpg',
+        resource: '外汇宝',
+        timestamp: _.dateFromNow(1497073010801),
+        count: 111
+      }, {
+        id: 32,
+        title: '比特币场内平台升级反洗钱系统场外监管政策空白有待填补',
+        img: 'http://m.anhuinews.com/upload/2017/06/06/201766631967.jpg',
+        resource: '外汇宝',
+        timestamp: _.dateFromNow(1497073010801),
+        count: 111
+      }, {
+        id: 33,
+        title: '比特币场内平台升级反洗钱系统场外监管政策空白有待填补',
+        img: 'http://m.anhuinews.com/upload/2017/06/06/201766631967.jpg',
+        resource: '外汇宝',
+        timestamp: _.dateFromNow(1497073010801),
+        count: 111
+      }
+    ],
+    errorMsg: '暂时没有数据哦~',
+    hasData: true,
+    hasMore: false,
+    showLoading: false
+  },
+  onLoad: function () {
+  },
+  onShareAppMessage: function () {
+    return {
+      title: '付勋' + '喊你来捡钱包啦！支付宝钱包、微信钱包都不如比特币钱包~',
+      path: '/pages/list/list',
+      success: function (res) {
+        // 转发成功
+        console.log(res);
+      },
+      fail: function (res) {
+        // 转发失败
+        console.log(res.errMsg);
+      }
+    };
+  }
+})
