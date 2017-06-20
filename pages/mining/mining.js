5601855a6695521c7d55f38198d8b339fb56538a
Xun.Fu <fuxun@xiaoyouzi.com>
2017-06-20 20:21:22 +0800

    添加挖矿原型UI。

--- pages/mining/mining.js
+++ pages/mining/mining.js
@@ -1,49 +1,30 @@
 //mining.js
 import _ from '../../utils/util.js';
 import API from '../../services/api';
+import Storage from '../../components/storage';
 
-const App = getApp();
-
-// let hour = 0;
-// let minute = 0;
-// let second = 0; //时 分 秒
-// let millisecond = 0; //毫秒
-// let int;
-// let flag = true;
+let stopwatchInterval;
+stopwatchInterval = 0;
 
 Page({
   data: {
-    // time: '00:00:00',
-    // coin: 0,
-    cid: 2,
-    articles: []
+    gold: '0.0000',
+    time: '00:00:00',
+    status: 'start' // start:开始，stop:停止
   },
   onLoad: function (options) {
     // 页面初始化 options为页面跳转所带来的参数
     // this.start();
     console.log('onLoad');
     const that = this;
-    _.showLoading();
-    that.fetchData(1, that.data.cid);
   },
   onReady: function() {
     // 页面渲染完成
     console.log('onReady');
+
   },
   onShow: function () {
-    // 监听页面显示
-    console.log('onShow');
-    // this.start();
-  },
-  onHide: function() {
-    // 监听页面隐藏
-    console.log('onHide');
-    // this.stop();
-  },
-  onUnload: function() {
-    // 监听页面卸载
-    console.log('onUnload');
-    // this.reset();
+
   },
   onShareAppMessage: function (options) {
     const name = App.globalData.WechatUser.nickName || App.globalData.defaultName;
@@ -60,78 +41,85 @@ Page({
       }
     };
   },
-  // reset: function() {
-  //   clearInterval(int);
-  //   millisecond = hour = minute = second = 0;
-  //   this.setData({
-  //     time: '00:00:00'
-  //   });
-  // },
-  // start: function() {
-  //   const self = this;
-  //   if (flag) {
-  //     flag = false;
-  //     int = setInterval(self.timer, 50);
-  //   }
-  // },
-  // timer: function() {
-  //   const self = this;
-  //   millisecond = millisecond + 50;
-  //   if (millisecond >= 1000) {
-  //     millisecond = 0;
-  //     second = second + 1;
-  //   }
-
-  //   if (second >= 60) {
-  //     second = 0;
-  //     minute = minute + 1;
-  //   }
-
-  //   if (minute >= 60) {
-  //     minute = 0;
-  //     hour = hour + 1;
-  //   }
-
-  //   if (hour >= 24) {
-  //     hour = 0;
-  //   }
-
-  //   this.setData({
-  //     time: [hour, minute, second].map(this.formatNumber).join(':')
-  //   });
-  // },
-  // formatNumber: function(n) {
-  //   n = n.toString();
-  //   return n[1] ? n : '0' + n;
-  // },
-  // stop: function() {
-  //   clearInterval(int);
-  //   flag = true;
-  // },
-  fetchData: function (page, cid) {
+  startWatch: function () {
     const that = this;
-    API.fetchIndex(page, cid).then(json => {
-      console.log('miningPage: ', JSON.stringify(json, null, 2));
-      _.hideLoading();
-      if (json && json.code === 0) {
-        const data = json.data;
-        data.article_list.forEach((item, index) => {
-          if (item.created_at <= 0) {
-            item.created_at = new Date().getTime();
-          }
-          item.created_at = _.msToDate(item.created_at, 'yyyy-MM-dd');
-        });
-        that.setData({
-          cid: data.cid,
-          articles: data.article_list
-        });
-      }
-    }, error => {
-      _.hideLoading();
+    clearInterval(stopwatchInterval);
+
+    const startTimestamp = new Date().getTime();
+    let runningTime = 0;
+    const str = Storage.readSync('stopwatchRunningTime');
+
+    Storage.writeSync('stopwatchBeginingTimestamp', startTimestamp);
+
+    if (Number(str)) {
+      runningTime = Number(str);
+    }
+    else {
+      Storage.writeSync('stopwatchRunningTime', 1);
+    }
+
+    stopwatchInterval = setInterval(function () {
+      const stopwatchTime = (new Date().getTime() - startTimestamp + runningTime);
+
       that.setData({
-        errorMsg: '咦，网络不见了，请检查网络连接后点击页面刷新~'
+        time: that.returnFormattedToMilliseconds(stopwatchTime)
       });
-      console.error('咦，网络不见了，请检查网络连接后点击页面刷新~', error);
+    }, 100);
+
+    that.setData({
+      status: 'stop'
     });
+  },
+  pauseWatch: function () {
+    const that = this;
+    clearInterval(stopwatchInterval);
+    const bt = Storage.readSync('stopwatchBeginingTimestamp');
+    const srt = Storage.readSync('stopwatchRunningTime');
+    console.log('stopwatchBeginingTimestamp: ', bt, ' stopwatchRunningTime', srt);
+    if (Number(bt)) {
+      const runningTime = Number(srt) + new Date().getTime() - Number(bt);
+
+      Storage.writeSync('stopwatchBeginingTimestamp', 0);
+      Storage.writeSync('stopwatchRunningTime', runningTime);
+
+      that.setData({
+        status: 'start'
+      });
+    }
+  },
+  resetWatch: function () {
+    const that = this;
+    clearInterval(stopwatchInterval);
+
+    Storage.writeSync('stopwatchBeginingTimestamp', 0);
+    Storage.writeSync('stopwatchRunningTime', 0);
+    that.setData({
+      time: that.returnFormattedToMilliseconds(0),
+      status: 'start'
+    });
+  },
+  formatNumber: function(n) {
+    n = n.toString();
+    return n[1] ? n : '0' + n;
+  },
+  returnFormattedToMilliseconds: function (time) {
+    const that = this;
+    const milliseconds = Math.floor((time % 1000) / 100);
+    const seconds = Math.floor((time / 1000) % 60);
+    const minutes = Math.floor((time / (1000 * 60)) % 60);
+    const hours = Math.floor((time / (1000 * 60 * 60)) % 24);
+
+    return that.formatNumber(hours) + ":" + that.formatNumber(minutes) + ":" + that.formatNumber(seconds) + "." + milliseconds;
+  },
+  goto: function (e) {
+    const that = this;
+    const ds = e.currentTarget.dataset;
+    const status = ds.status;
+    console.log('status: ', status);
+    if (status === 'start') {
+      that.startWatch();
+    } else {
+      that.pauseWatch();
+    }
   }
 });
