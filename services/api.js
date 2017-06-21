7d59d73641e953cbd736ef72a8b0a8c5165b1da9
Xun.Fu <fuxun@xiaoyouzi.com>
2017-06-21 20:29:16 +0800

    删除多余的接口信息。

--- services/api.js
+++ services/api.js
@@ -6,7 +6,6 @@ const WX_URL = `https://api.damabtc.com`;
 
 const API_METHODS = {
   'user': `${WX_URL}/wx/connect`,
-  'userSetting': `${WX_URL}/wx/mode`,
   'index': `${WX_URL}/article/list`,
   'detail': `${WX_URL}/article/detail`,
   'mineGet': `${WX_URL}/mine/get`,
@@ -24,54 +23,6 @@ const authorize = (code, iv, info) => {
         iv: iv,
         info: info
       }
-    }).then(data => {
-      resolve(data);
-    }, err => {
-      reject(err);
-    });
-  });
-};
-
-const putUserInfo = (data) => {
-  return new Promise((resolve, reject) => {
-    const url = API_METHODS['userSetting'];
-
-    const mode = parseInt(data.mode, 10);
-    const circle = data.menstrual_cycle;
-    const duration = data.duration_of_menstruation;
-    const lastMenses = data.last_menses;
-    const duedate = data.duedate;
-    const babyBirthday = data.baby_birthday;
-    const babySex = data.baby_sex;
-    let dataObj = {};
-
-    if (mode === 1) { // 怀孕
-      dataObj = {
-        mode: 1,
-        duration_of_menstruation: parseInt(duration) || 5,
-        menstrual_cycle: parseInt(circle) || 28,
-        last_menses: lastMenses || '',
-        duedate: duedate || ''
-      };
-    } else if (mode === 2) { // 备孕
-      dataObj = {
-        mode: 2,
-        duration_of_menstruation: parseInt(duration) || 5,
-        menstrual_cycle: parseInt(circle) || 28,
-        last_menses: lastMenses || '',
-        duedate: duedate || ''
-      };
-    } else if (mode === 3) { // 辣妈
-      dataObj = {
-        mode: 3,
-        baby_birthday: babyBirthday || '',
-        baby_sex: babySex + 1 || 0
-      };
-    }
-
-    Http.instance.put({
-      url,
-      data: dataObj
     }).then(data => {
       resolve(data);
     }, err => {
@@ -161,6 +112,5 @@ module.exports = {
   handleMineGet,
   handleMineGo,
   handleMineStop,
-  authorize,
-  putUserInfo
+  authorize
 };
