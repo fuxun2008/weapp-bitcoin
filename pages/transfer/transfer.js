295ddff17ae5f4b0bbf68e98ac3969226c037e8c
Xun.Fu <fuxun@xiaoyouzi.com>
2017-06-25 01:16:45 +0800

    套接转账页面接口数据。

--- pages/transfer/transfer.js
+++ pages/transfer/transfer.js
@@ -6,9 +6,28 @@ const App = getApp();
 
 Page({
   data: {
+    walletId: ''
   },
   onLoad: function () {
     const that = this;
+  },
+  handleScan: function () {
+    const that = this;
+    wx.scanCode({
+      onlyFromCamera: false,
+      success: function (res) {
+        console.log('res: ', JSON.stringify(res, null, 2));
+        that.setData({
+          walletId: res.result
+        });
+      },
+      fail: function (res) {
+        console.error('res: ', JSON.stringify(res, null, 2));
+        that.setData({
+          walletId: ''
+        });
+      }
+    });
   },
   formSubmit: function (e) {
     console.log('form发生了submit事件，携带数据为：', e.detail.value)
